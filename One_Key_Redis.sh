211b10c26acf68d0d6fbceda674024e739366773
Dong Zihao <13835518617@163.com>
2020-01-17 09:01:45 +0800

    install redis cluster :1

--- One_Key_Redis.sh
+++ One_Key_Redis.sh
@@ -1,61 +1,47 @@
 #!/bin/bash
 # Need SSH password free login required
-# Configure yum is YES
+# Configure yum is TURE
 # redis-4.0.8.tar.gz place to /root/redis/
+# 192.168.4.57 is manager host
 
-GCC (){
-for i in {51..59}
-do
-	ssh 192.168.4.$i yum -y install gcc &> /dev/null
+CHECK (){
 	if [ $? -eq 0 ] ;then
-		echo "GCC$i YES"
+		echo "$1--->$i  -----------------------------------------YES"
 	else
-		echo "GCC$i NO"
+		echo "$1--->$i  -----------------------------------------NO"
 	fi
-done
 }
 
-TAR_REDIS (){
-for i in {51..59}
+
+for a in {51..59}
 do
-	ssh 192.168.4.$i tar -xf /root/redis/redis-4.0.8.tar.gz -C /root
-	if [ $? -eq 0 ] ;then
-		echo "TAR_REDIS$i YES"
+	if [ $a -eq 57 ] ;then
+		continue
 	else
-		echo "TAR_REDIS$i NO"
+		ssh 192.168.4.$a yum -y install gcc &> /dev/null
 	fi
-done
-}
+done &
 
-MAKE (){
 for i in {51..59}
 do
-	ssh 192.168.4.$i make -C /root/redis-4.0.8
-	if [ $? -eq 0 ] ;then
-		echo "MAKE$i YES"
+	if [ $i -eq 57 ] ;then
+		continue
 	else
-		echo "MAKE$i NO"
-	fi
-done
-}
-
-MAKE_INSTALL (){
-for i in {51..59}
-do
-	ssh 192.168.4.$i make install -C /root/redis-4.0.8
-	if [ $? -eq 0 ] ;then
-		echo "MAKE_INSTALL$i YES"
-	else
-		echo "MAKE_INSTALL$i NO"
-	fi
-done
-}
-
-INIT (){
-for i in {51..59}
-do
-	ssh 192.168.4.$i /root/redis-4.0.8/utils/install_server.sh<<EOF
-
+		while :
+		do
+			sleep 1
+			ssh 192.168.4.$i rpm -q gcc &> /dev/null
+			if [ $? -eq 0 ] ;then
+				break
+			fi
+		done
+		ssh 192.168.4.$i tar -xf /root/redis/redis-4.0.8.tar.gz -C /root
+		CHECK "tar -xf"
+		ssh 192.168.4.$i make -C /root/redis-4.0.8
+		CHECK "make"
+		ssh 192.168.4.$i make install -C /root/redis-4.0.8
+		CHECK "make install"
+		ssh 192.168.4.$i /root/redis-4.0.8/utils/install_server.sh<<EOF
 
 
 
@@ -64,65 +50,21 @@ do
 
 
 EOF
-	if [ $? -eq 0 ] ;then
-		echo "INIT$i YES"
-	else
-		echo "INIT$i NO"
+		CHECK "init"
+		ssh 192.168.4.$i sed -ri "93s/6379/63$i/" /etc/redis/6379.conf
+		ssh 192.168.4.$i sed -ri "70s/127.0.0.1/192.168.4.$i/" /etc/redis/6379.conf
+		ssh 192.168.4.$i "redis-cli shutdown"
+		CHECK "shutdown"
+	        ssh 192.168.4.$i "redis-server /etc/redis/6379.conf"
+		CHECK "start"
 	fi
 done
-}
 
-PORT () {
-for i in {51..59}
+for j in {51..59}
 do
-	ssh 192.168.4.$i sed -ri "93s/6379/63$i/" /etc/redis/6379.conf
-	if [ $? -eq 0 ] ;then
-		echo "PORT$i YES"
+	if [ $j -eq 57 ];then
+		continue
 	else
-		echo "PORT$i NO"
+		ssh 192.168.4.$i "ss -antup |grep :63"
 	fi
 done
-}
-
-BIND () {
-for i in {51..59}
-do
-	ssh 192.168.4.$i sed -ri "70s/127.0.0.1/192.168.4.$i/" /etc/redis/6379.conf
-	if [ $? -eq 0 ] ;then
-		echo "BIND$i YES"
-	else
-		echo "BIND$i NO"
-	fi
-done
-}
-
-RESTART () {
-for i in {51..59}
-do
-	ssh 192.168.4.$i "redis-cli -h 192.168.4.$i -p 63$i shutdown"
-        ssh 192.168.4.$i "redis-server /etc/redis/6379.conf"
-	if [ $? -eq 0 ] ;then
-		echo "RESTART$i YES"
-	else
-		echo "RESTART$i NO"
-	fi
-done
-}
-
-CHECK () {
-for i in {51..59}
-do
-	ssh 192.168.4.$i ss -antup |grep :63
-done
-}
-
-
-GCC
-TAR_REDIS
-MAKE
-MAKE_INSTALL
-INIT
-PORT
-BIND
-RESTART
-CHECK
